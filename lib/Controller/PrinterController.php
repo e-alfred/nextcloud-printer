cdc94d2db7a59b9cd6c7f806ee7297546316699f
Alfred Egger <aegger@cosy.sbg.ac.at>
2019-06-19 10:44:41 +0200

    Revert "Fix API call"
    This reverts commit c03580bfb3c65743c74aae859066764f8e9656df.

--- lib/Controller/PrinterController.php
+++ lib/Controller/PrinterController.php
@@ -28,7 +28,7 @@ class PrinterController extends Controller {
 		 */
 	  public function printfile($sourcefile, $orientation) {
 	  		if($orientation === "landscape") {
-					$filefullpath = Filesystem::getLocalFile($sourcefile);
+					$filefullpath = \OC\Files\Filesystem::getLocalFile($sourcefile);
 					exec('lpr ' . $filefullpath);
 	  			return new JSONResponse(
 							array(
